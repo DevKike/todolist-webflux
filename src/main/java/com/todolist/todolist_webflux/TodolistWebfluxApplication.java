4e42cf8948169cca23bc81f5b040bcdb004238c8
DevKike <alvaronarvaez.dev@gmail.com>
2025-08-09 22:01:43 -0500

    Feat: add init files

---
+++ src/main/java/com/todolist/todolist_webflux/TodolistWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.todolist_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/todolist_webflux/TodolistWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.todolist_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
